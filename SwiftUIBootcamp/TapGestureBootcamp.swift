673dac13d1d40b563eca294d130361539043ef67
Waseem Mohammad <wmohammad83@icloud.com>
2023-07-26 21:19:20 +0100

    TapGestures file created with code to function on 1 or 2 taps

---
+++ SwiftUIBootcamp/TapGestureBootcamp.swift
@@ -0,0 +1,57 @@
+//
+//  TapGestureBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Waseem Mohammad on 26/07/2023.
+//
+
+import SwiftUI
+
+struct TapGestureBootcamp: View {
+    
+    @State var isSelected: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 40){
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(height: 200)
+                .foregroundColor(isSelected ? Color.green : Color.red)
+            
+            Button {
+                isSelected.toggle()
+            } label: {
+                Text("Button")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(25)
+            }
+            
+            Text("Tap Gesture")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(25)
+//                .onTapGesture {
+//                    isSelected.toggle()
+//                }
+                .onTapGesture(count: 2) {
+                    isSelected.toggle()
+                }
+            
+            Spacer()
+
+        }
+        .padding(40)
+    }
+}
+
+struct TapGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureBootcamp()
+    }
+}
